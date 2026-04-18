43870a63dad283c5228f7eaa589dcfbe8427b603
softgenai <admin@softgen.ai>
2026-04-18 08:37:25 +0000

    feat(ui): add product editing features to side sheet

--- src/pages/explore/[id].tsx
+++ src/pages/explore/[id].tsx
@@ -1152,7 +1152,8 @@ export default function ExploreStorePage() {
                                 </TableRow>
                               )}
                             </>
-                          ))
+                          );
+                          })
                         )}
                       </TableBody>
                     </Table>
