83230b7a7c85a4d4dbc3355380b9a6ffb6013a2a
softgenai <admin@softgen.ai>
2026-04-18 13:20:46 +0000

    Revert to state at ff2640ecd7ec568e93d51ed26ee1e6ec289a12e1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { BrandingProvider } from "@/contexts/BrandingProvider";
 import { AuthProvider } from "@/contexts/AuthProvider";
-import { AppShell } from "@/components/layout/AppLayout";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const isAuthPage = router.pathname.startsWith("/auth/");
   return (
     <ThemeProvider>
       <AuthProvider>
         <BrandingProvider>
-          {isAuthPage ? (
-            <Component {...pageProps} />
-          ) : (
-            <AppShell>
-              <Component {...pageProps} />
-            </AppShell>
-          )}
+          <Component {...pageProps} />
           <Toaster />
         </BrandingProvider>
       </AuthProvider>
